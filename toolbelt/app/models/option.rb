903e6f5d90ff162036a78270bd5533200117ed3c
Peter Lowe <peter.lowe42@gmail.com>
2016-03-11 20:44:45 -0800

    Add searchable page and key fields to options

--- toolbelt/app/models/option.rb
+++ toolbelt/app/models/option.rb
@@ -4,5 +4,7 @@ class Option
   include Mongoid::Tree::Ordering
   field :text, type: String
   field :head, type: String
+  field :page, type: String
+  field :key, type: String
 end
 
--- toolbelt/db/seeds.rb
+++ toolbelt/db/seeds.rb
@@ -1,5 +1,7 @@
 Option.destroy_all
+Glossary.destroy_all
 
+@curent_page = 'index'
 # key_site variable should be dynamically created and sent in
 key_site = '/Users/apprentice/Desktop/pete/Jepson-2/ucjeps.berkeley.edu/IJM_fam_key.html'
 def scrape(key_site)
@@ -13,9 +15,9 @@ end
 def make_first_nodes(dichotomies)
   dichotomies.each do |dichotomy|
     if dichotomy[0] == "1" && dichotomy[1] == '.'
-      first = Option.create(text:dichotomy, head: "root")
+      first = Option.create(text:dichotomy,page: @current_page, head: "root", key: "1.")
     elsif dichotomy[0] == "1" && dichotomy[1] == "'"
-      Option.first.siblings << Option.create(text:dichotomy, head: "root")
+      Option.first.siblings << Option.create(text:add_tool_tip_span(dichotomy),page: @current_page, head: "root", key: "1'")
     end
   end
 end
@@ -26,11 +28,11 @@ def fill_tree(dichotomies)
     prime_match = (/^#{Regexp.quote(i.to_s)}'/)
     non_prime_match = (/^#{Regexp.quote(i.to_s)}\./)
     parent_index = dichotomies.find_index {|dic| dic.match(non_prime_match)} - 1 # finds the index the current option, and subtracts one to find it's parent, this is an inherent pattern to dichotomous keys.
-    text = dichotomies.find {|dic| dic.match(non_prime_match)} #adds the text of the option to the variable.
-    parent = Option.find_by(text: dichotomies[parent_index]) #finds the parent object using the parent_index variable assigned earlier.
-    parent.children << Option.create(text: text) #creates a new instance of an option and adding it as a child of the parent
+    @text = dichotomies.find {|dic| dic.match(non_prime_match)} #adds the text of the option to the variable.
+    parent = Option.find_by(page: @curent_index, key: dichotomies[parent_index][/^([^\s]+)/]) #finds the parent object using the parent_index variable assigned earlier.
+    parent.children << Option.create(text: add_tool_tip_span(@text),page: @current_page, key: "#{i}.") #creates a new instance of an option and adding it as a child of the parent
     text_prime = dichotomies.find {|dic| dic.match(prime_match)} # finds the prime version, of the recently created Option instance.
-    parent.children << Option.create(text: text_prime) # Adds previous line as a child of the parent.
+    parent.children << Option.create(text: text_prime, page: @current_page, key: "#{i}'") # Adds previous line as a child of the parent.
     i += 1
   end
 end
@@ -49,7 +51,7 @@ end
 
 def parse_words(glossary_doc)
   @words = []
-  @glossary_doc.css('dt').each {|x| @words << x.inner_text}
+  @glossary_doc.css('dt').each {|x| @words << x.inner_text.chomp('.')}
   return @words
 end
 
@@ -63,14 +65,27 @@ def create_glossary_record
   end
 end
 
+def add_tool_tip_span(text)
+  text_array = text.split(" ")
+  text_array.each do |word|
+    if Glossary.where(word: word)[0]
+      entry = Glossary.find_by(word: word)
+      word = "<span data-tooltip aria-haspopup='true' class='has-tip [tip-top tip-bottom tip-left tip-right] [radius round]' title=#{entry.definition}>#{word}</span>"
+    end
+  end
+  return text_array.join(" ")
+end
 
-dichotomies = scrape(key_site)
-make_first_nodes(dichotomies)
-fill_tree(dichotomies)
+#3. Sporangia, sporangium cases, seeds, cones, or cone-like structures 0 or not readily apparent [plants in strictly vegetative condition]
+#3.  Sporangia, sporangium cases, seeds, cones, or cone-like structures 0 or not readily apparent [plants in strictly vegetative condition]
 nokogiri_glossary
 parse_definitions(@glossary_doc)
 parse_words(@glossary_doc)
 create_glossary_hash
 create_glossary_record
+dichotomies = scrape(key_site)
+make_first_nodes(dichotomies)
+fill_tree(dichotomies)
+
 
 
