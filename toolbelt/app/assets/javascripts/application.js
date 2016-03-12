7a1c57c313f7f6e7574cc7554d7c8dbf8311d37a
Timur Catakli <timurcatakli@gmail.com>
2016-03-11 18:02:37 -0800

    Complete frontend integration

--- toolbelt/app/assets/javascripts/application.js
+++ toolbelt/app/assets/javascripts/application.js
@@ -11,9 +11,31 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+$(function(){ 
+  
+
+  $(document).foundation();
+  $('#arrowLeft').click(function(){
+    $('#dataCarousel').animate({'margin-left': '-=1720px'}, 1000);
+    // make an ajax request to root/options/12876876
+    // receive a JSON
+    // json = [{id: 123123123, text: "sdfsdfsdfds"}, {id: 123123123, text: "sdfsdfsdfds"}]
+    // populate the divs using JSON data
+
+
+  });
+
+  $('#arrowRight').click(function(){
+    $('#dataCarousel').animate({'margin-right': '-=1720px'}, 1000);
+    $('#dataCarousel').show()
+  });
+  
+
+
+});
