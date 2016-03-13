2358b94c323db958dc03c657bac49d339e99e8e4
DaveSpivey <dspive1@gmail.com>
2016-03-13 10:08:24 -0700

    Add feature specs for navigating carousel

--- spec/features/option_display_spec.rb
+++ spec/features/option_display_spec.rb
@@ -3,27 +3,39 @@ require 'spec_helper'
 
 describe Option, :js => true do
 
-  let(:option) { Option.create(text: "Is it a plant?", head: "root") }
-  let(:option_prime) { Option.create(text: "Is it a dog?", head: "root") }
-  # let(:options) { Option.where(head: 'root') }
+  before(:all) { Option.destroy_all }
+
+  let!(:option) { Option.create(text: "Is it a plant?", head: "root") }
+  let!(:option_prime) { Option.create(text: "Is it a dog?", head: "root") }
+  let!(:child) { option.children.create(text: "Bulblets") }
+  let!(:child_prime) { option.children.create(text: "That's right, Bulblets") }
+  let!(:dog) { option_prime.children.create(text: "Maybe it's a dogplant?") }
+  let!(:dog_prime) { option_prime.children.create(text: "It's almost certainly a dogplant.") }
+
+  before(:each) { visit root_path }
 
   context 'a user' do
     it 'can see the page title' do
-      visit root_path
+      p "SPEC " + ("$" * 80)
+      p option
+      p option.children
       expect(page).to have_content("welcome to Botanist Toolbelt")
     end
 
     describe 'option display' do
-      # let(:arrow) { page.find_link('#view-index') }
-      it "renders the option template" do
-        # click_on arrow
-        # within ".item" do
-        #   first(:link, "Agree").click
-        # end
-        # click_on('&#9664; Continue Here')
+      it "renders the option template on left navigation" do
         click_on('◀ Continue Here')
-        expect(page).to render_template("layouts/carousel")
+        expect(page.find("#dataCarousel")).to have_content("Bulblets")
       end
+
+      it "renders the option template on right navigation" do
+        click_on('Continue Here ▶')
+        expect(page.find("#dataCarousel")).to have_content("dogplant")
+      end
+      # it "renders links to navigate further" do
+      #   # render :template => "layouts/carousel.html"
+      #   rendered.should include("◀ Continue Here")
+      # end
     end
   end
 
