058ae34f4ecc219a33ec1daa81b897aafe6105df
Reuben Brandt <email": null>
2016-03-13 13:37:15 -0700

    Further RuboCop editing

--- app/helpers/scrape_helpers.rb
+++ app/helpers/scrape_helpers.rb
@@ -45,9 +45,9 @@ def fill_tree(dichotomies, current_href)
     prime_match = /^#{Regexp.quote(i.to_s)}'/
     non_prime_match = /^#{Regexp.quote(i.to_s)}\./
     parent_index = dichotomies.find_index { |dic| dic.match(non_prime_match) } - 1
-    @text = dichotomies.find { |dic| dic.match(non_prime_match) }
+    text = dichotomies.find { |dic| dic.match(non_prime_match) }
     parent = Option.find_by(page: current_href, key: dichotomies[parent_index][/^([^\s]+)/])
-    parent.children << Option.create(text: add_tool_tip_span(@text), page: current_href, key: "#{i}.")
+    parent.children << Option.create(text: add_tool_tip_span(text), page: current_href, key: "#{i}.")
     text_prime = dichotomies.find { |dic| dic.match(prime_match) }
     parent.children << Option.create(text: text_prime, page: current_href, key: "#{i}'")
     i += 1
@@ -57,6 +57,7 @@ end
 def nokogiri_glossary
   glossary_uri = URI('http://ucjeps.berkeley.edu/eflora/glossary.html')
   @glossary_doc = Nokogiri.parse(Net::HTTP.get(glossary_uri))
+  @glossary_doc
 end
 
 def parse_definitions
@@ -82,15 +83,15 @@ def create_glossary_record
 end
 
 def add_tool_tip_span(text)
-  text_array = text.split(" ")
+  text_array = text.split(' ')
   text_array.each do |word|
-    search_word = word.chomp(",").chomp(".")
+    search_word = word.chomp(',').chomp('.')
     if Glossary.where(word: search_word)[0]
       entry = Glossary.find_by(word: search_word)
-      text.gsub!(/#{Regexp.quote(word)}/, "<span data-tooltip aria-haspopup='true' class='has-tip [tip-top tip-bottom tip-left tip-right] [radius round]' title=#{entry.definition}>#{word}</span>")
-    elsif Glossary.where(word: search_word.chomp("s"))[0]
-      entry = Glossary.find_by(word: search_word.chomp("s"))
-      text.gsub!(/#{Regexp.quote(word)}/, "<span data-tooltip aria-haspopup='true' class='has-tip [tip-top tip-bottom tip-left tip-right] [radius round]' title=#{entry.definition}>#{word}</span>")
+      text.gsub!(/#{Regexp.quote(word)}/, "<span data-tooltip aria-haspopup='true' class='has-tip [tip-top tip-bottom tip-left tip-right] [radius round]' title=#{ entry.definition }>#{ word }</span>")
+    elsif Glossary.where(word: search_word.chomp('s'))[0]
+      entry = Glossary.find_by(word: search_word.chomp('s'))
+      text.gsub!(/#{Regexp.quote(word)}/, "<span data-tooltip aria-haspopup='true' class='has-tip [tip-top tip-bottom tip-left tip-right] [radius round]' title=#{e ntry.definition }>#{ word }</span>")
     end
   end
 
@@ -101,23 +102,22 @@ def find_links(url)
   uri = URI(url)
   doc = Nokogiri.parse(Net::HTTP.get(uri))
   links = doc.css('blockquote').css('a')
-  hrefs = links.map {|link| (link.attribute("href").to_s)}
-  useful_hrefs = hrefs.select {|href| href.match(/^\/.*/)}
-  return useful_hrefs
+  hrefs = links.map { |link| (link.attribute('href').to_s) }
+  useful_hrefs = hrefs.select { |href| href.match(/^\/.*/) }
+  useful_hrefs
 end
 
 
 def find_link_parent_keys(url)
   p_tags = scrape(url)
-  tags_with_links = p_tags.select { |txt| txt.match(/(?<=\.\.\.\.\.).*/)}
-  parent_keys = tags_with_links.map {|txt| txt.gsub(/\s.+/, '')}
-  return parent_keys
+  tags_with_links = p_tags.select { |txt| txt.match(/(?<=\.\.\.\.\.).*/) }
+  parent_keys = tags_with_links.map { |txt| txt.gsub(/\s.+/, '') }
+  parent_keys
 end
 
 def make_link_hash(url)
-  url
   links = find_links(url)
   keys = find_link_parent_keys(url)
   link_hash = Hash[keys.zip links]
-  return link_hash
+  link_hash
 end
