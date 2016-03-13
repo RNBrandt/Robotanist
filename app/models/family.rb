46d40be246571278afacbca1912ad0f3e6dd9566
Peter Lowe <peter.lowe42@gmail.com>
2016-03-13 11:50:14 -0700

    Add Family Genus and Species classes along with methods to create instances

---
+++ app/models/family.rb
@@ -0,0 +1,8 @@
+class Family
+  include Mongoid::Document
+  include Mongoid::Tree
+  include Mongoid::Tree::Ordering
+  field :scientific_name, type: String
+  field :common_name, type: String
+  field :description, type: String
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -49,7 +49,37 @@ def recursive_scrape(href, parent_page=nil,parent_key=nil)
 
 end
 
+def assign_obj_type(url)
+  uri = URI(url)
+  doc = Nokogiri.parse(Net::HTTP.get(uri))
+  main_heading = doc.css("span.pageLargeHeading").inner_text
+  sub_heading = doc.css("span.pageMajorHeading").inner_text
+  if main_heading.split(" ").length >= 2
+    return create_obj(url, Species)
+  elsif sub_heading.match(/.+FAMILY/)
+    return create_obj(url, Family)
+  else
+    return create_obj(url, Genus)
+  end
+
+end
 
 
-recursive_scrape(@current_href)
+def create_obj(url, klass)
+  uri = URI(url)
+  doc = Nokogiri.parse(Net::HTTP.get(uri))
+  scientific_name = doc.css("span.pageLargeHeading").inner_text
+  common_name = doc.css("span.pageMajorHeading").inner_text
+  description = doc.css("div.bodyText").inner_text.match(/.+?(?=eFlora)/)[0]
+  return klass.create(scientific_name: scientific_name, common_name: common_name, description: description)
+end
+
+
+
+p assign_obj_type('http://ucjeps.berkeley.edu/eflora/eflora_display.php?tid=33989')
+
+p assign_obj_type("http://ucjeps.berkeley.edu/eflora/eflora_display.php?tid=34")
+
+p assign_obj_type("http://ucjeps.berkeley.edu/eflora/eflora_display.php?tid=10193")
+
 
