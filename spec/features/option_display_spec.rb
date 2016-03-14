bbd8a990103895616ef684a6149c02c4414bf6f9
DaveSpivey <dspive1@gmail.com>
2016-03-14 15:21:07 -0700

    Update feature tests for new layout

--- spec/features/option_display_spec.rb
+++ spec/features/option_display_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 
 describe Option, :js => true do
 
-  before(:all) { Option.destroy_all }
+  # before(:all) { Option.destroy_all }
 
-  let!(:option) { Option.create(text: "Is it a plant?", head: "root") }
-  let!(:option_prime) { Option.create(text: "Is it a dog?", head: "root") }
-  let!(:child) { option.children.create(text: "Bulblets") }
-  let!(:child_prime) { option.children.create(text: "That's right, Bulblets") }
-  let!(:dog) { option_prime.children.create(text: "Maybe it's a dogplant?") }
-  let!(:dog_prime) { option_prime.children.create(text: "It's almost certainly a dogplant.") }
+  # let!(:option) { Option.create(text: "Is it a plant?", head: "root") }
+  # let!(:option_prime) { Option.create(text: "Is it a dog?", head: "root") }
+  # let!(:child) { option.children.create(text: "Bulblets") }
+  # let!(:child_prime) { option.children.create(text: "That's right, Bulblets") }
+  # let!(:dog) { option_prime.children.create(text: "Maybe it's a dogplant?") }
+  # let!(:dog_prime) { option_prime.children.create(text: "It's almost certainly a dogplant.") }
 
   before(:each) {
     Capybara.current_window.resize_to(1400,900)
@@ -19,9 +19,6 @@ describe Option, :js => true do
 
   context 'index page' do
     it 'displays page title' do
-      p "SPEC " + ("$" * 80)
-      p option
-      p option.children
       expect(page).to have_content("welcome to Botanist Toolbelt")
     end
 
@@ -33,19 +30,22 @@ describe Option, :js => true do
 
       it "renders the option template on right navigation" do
         click_on('Continue Here ▶')
-        expect(page.find("#dataCarousel")).to have_content("dogplant")
+        expect(page.find("#dataCarousel")).to have_content("Specimens")
       end
     end
 
     describe 'info tab' do
       it "loads info from Wikipedia API" do
-        expect(page.find('#tabDetail')).to have_content("Latin")
+        click_on('Angiosperm')
+        expect(page.find('#wikipediaDiv')).to have_content("angiosperm")
       end
     end
 
     describe 'photo gallery' do
       it "displays images from Flickr API" do
-        expect(page.find('#flickr_photos')).to have_selector('img')
+        click_on('Angiosperm')
+        click_on('Flickr Photos')
+        expect(page.find('#flickrDiv')).to have_selector('img')
       end
     end
   end
