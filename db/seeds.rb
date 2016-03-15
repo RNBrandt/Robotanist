d1b33d93bf405aa8f5bbbe8d98245bb7a6f76b6f
DaveSpivey <dspive1@gmail.com>
2016-03-15 15:22:31 -0700

    Apply layout to families and genera pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,11 @@ require_relative "../app/helpers/link_helpers"
 require_relative "../app/helpers/object_helpers"
 require_relative "../app/helpers/glossary_helpers"
 require_relative "../app/helpers/blockquote_class"
-# Option.destroy_all
-# Glossary.destroy_all
-# Species.destroy_all
-# Family.destroy_all
-# Genus.destroy_all
+Option.destroy_all
+Glossary.destroy_all
+Species.destroy_all
+Family.destroy_all
+Genus.destroy_all
 
 BASE_URL = "http://ucjeps.berkeley.edu"
 
