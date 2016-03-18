51948048172477f0ebca2f49a3f10eaed4cb764b
DaveSpivey <dspive1@gmail.com>
2016-03-18 15:49:11 -0700

    Add fix for search

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,14 +14,14 @@ BASE_URL = "http://ucjeps.berkeley.edu"
 
 @root_href = "/IJM_fam_key.html"
 
-# big_family_hash = {"Asteraceae"=> "/cgi-bin/get_IJM.pl?key=58","Brassicaceae"=>"/cgi-bin/get_IJM.pl?key=70", "Fabaceae"=> "/cgi-bin/get_IJM.pl?key=134", "Poaceae"=> "/cgi-bin/get_IJM.pl?key=223"}
+big_family_hash = {"Asteraceae"=> "/cgi-bin/get_IJM.pl?key=58","Brassicaceae"=>"/cgi-bin/get_IJM.pl?key=70", "Fabaceae"=> "/cgi-bin/get_IJM.pl?key=134", "Poaceae"=> "/cgi-bin/get_IJM.pl?key=223"}
 
-# first_blockquote = get_blockquote("/IJM_fam_key.html")
-# parser = BlockQuoteParser.new(first_blockquote, "/IJM_fam_key.html")
+first_blockquote = get_blockquote("/IJM_fam_key.html")
+parser = BlockQuoteParser.new(first_blockquote, "/IJM_fam_key.html")
 create_glossary
-# recursive_scrape(parser)
-# scrape_from_families
-# big_family_hash.each do |name,href|
-#   big_family_scraper(href, name)
-# end
-p add_tool_tip_span("8' Specimens with bisexual flowers or with both staminate and pistillate flowers on same or different individuals; plants bisexual, dioecious, or monoecious (occasionally with mixture of bisexual and unisexual flowers)")
+recursive_scrape(parser)
+scrape_from_families
+big_family_hash.each do |name,href|
+  big_family_scraper(href, name)
+end
+
