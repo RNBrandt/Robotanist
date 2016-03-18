008ee1dbf012eecc54c748ca2de4c17be7b36fd7
Reuben Brandt <email": null>
2016-03-18 08:35:32 -0700

    A few small formatting bits in the _nav, application, and seeds files

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,11 @@ require_relative "../app/helpers/object_helpers"
 require_relative "../app/helpers/glossary_helpers"
 require_relative "../app/helpers/blockquote_class"
 
-Option.destroy_all
-Glossary.destroy_all
-Species.destroy_all
-Family.destroy_all
-Genus.destroy_all
+#Option.destroy_all
+#Glossary.destroy_all
+#Species.destroy_all
+#Family.destroy_all
+#Genus.destroy_all
 
 
 BASE_URL = "http://ucjeps.berkeley.edu"
@@ -49,11 +49,11 @@ def big_family_scraper(href)
 
 end
 
-big_family_hash = {"Asteraceae"=> "/cgi-bin/get_IJM.pl?key=58","Brassicaceae"=>"/cgi-bin/get_IJM.pl?key=70", "Fabaceae"=> "/cgi-bin/get_IJM.pl?key=134", "Poaceae"=> "/cgi-bin/get_IJM.pl?key=223"}
+#big_family_hash = {"Asteraceae"=> "/cgi-bin/get_IJM.pl?key=58","Brassicaceae"=>"/cgi-bin/get_IJM.pl?key=70", "Fabaceae"=> "/cgi-bin/get_IJM.pl?key=134", "Poaceae"=> "/cgi-bin/get_IJM.pl?key=223"}
 
-first_blockquote = get_blockquote("/IJM_fam_key.html")
-parser = BlockQuoteParser.new(first_blockquote, "/IJM_fam_key.html")
-create_glossary
+#first_blockquote = get_blockquote("/IJM_fam_key.html")
+#parser = BlockQuoteParser.new(first_blockquote, "/IJM_fam_key.html")
+#create_glossary
 recursive_scrape(parser)
 scrape_from_families
 big_family_hash.each do |name,href|
